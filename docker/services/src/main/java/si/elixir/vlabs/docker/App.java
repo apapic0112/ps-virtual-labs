a2cdcc23e1605e4ad5257f9a9016a07800df6f58
Aleš Papič <ales.papic94@gmail.com>
2017-11-23 08:36:57 +0100

    Included travis and docker config files

--- docker/services/src/main/java/si/elixir/vlabs/docker/App.java
+++ docker/services/src/main/java/si/elixir/vlabs/docker/App.java
@@ -1,21 +1,8 @@
 package si.elixir.vlabs.docker;
 
-import com.github.dockerjava.api.DockerClient;
-import com.github.dockerjava.api.command.ListContainersCmd;
-import com.github.dockerjava.api.model.Container;
-import com.github.dockerjava.core.DockerClientBuilder;
-
-import java.util.List;
-
 public class App {
 
     public static void main(String[] args) {
-        DockerClient instance = DockerClientBuilder.getInstance().build();
 
-        // "running"
-        ListContainersCmd running = instance.listContainersCmd().withShowAll(true);
-        List<Container> containers = running.exec();
-
-        System.out.print("");
     }
 }
