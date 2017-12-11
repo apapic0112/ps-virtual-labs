64acb51b97eb6d660700b890efcb2eb30f14b201
Aleš Papič <ales.papic94@gmail.com>
2017-12-11 22:22:10 +0100

    Minor changes...

--- travis.sh
+++ travis.sh
@@ -8,6 +8,3 @@ docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
 
 docker build -t "$DOCKER_IMAGE_MS_DOCKER" ./docker/
 docker push "$DOCKER_IMAGE_MS_DOCKER"
-# - LATEST -
-docker build -t "$DOCKER_IMAGE_MS_DOCKER_LATEST" ./docker/
-docker push "$DOCKER_IMAGE_MS_DOCKER_LATEST"
